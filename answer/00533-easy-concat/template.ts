0bb220fd3e5a8641bb1d83a9b56c080eae814da2
NTBrave <1398811892@qq.com>
2022-08-27 10:56:08 +0800

    feat: 做题533 concat (终于自己做一个出来了）

---
+++ answer/00533-easy-concat/template.ts
@@ -0,0 +1,8 @@
+
+
+/**
+ * 1. type Concat<T extends any[], U extends any[]> = [...T, ...U]
+ *
+ * 虽然也可以正常通过。不过其实不太好，因为对于未知类型，尽量用unknown
+ */
+type Concat<T extends unknown[], U extends unknown[]> =  [...T, ...U]
