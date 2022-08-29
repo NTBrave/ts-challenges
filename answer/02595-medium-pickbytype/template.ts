402b7166ce258137732df87ee0f03276dad942d7
caidl6860 <caidl@yeastar.com>
2022-08-29 17:34:56 +0800

    feat: 做题9，深度readonly

--- answer/02595-medium-pickbytype/template.ts
+++ answer/02595-medium-pickbytype/template.ts
@@ -1,5 +1,5 @@
 // 重点 P in keyof T 是一组， as  any extends P 是为了创造条件
-// 最后黄算下来是 P in keyof T as P 的意思！
+// 最后算下来是 P in keyof T as P 的意思！
 type PickByType<T, U> = {
   [P in keyof T as any extends P ? never :  T[P] extends U ? P : never]: T[P];
 };
