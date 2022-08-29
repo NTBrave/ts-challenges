0649c16b4f329b7a7d0c67122b2030ae3d4ed48a
caidl6860 <caidl@yeastar.com>
2022-08-29 17:44:32 +0800

    feat: 做题10

---
+++ answer/00010-medium-tuple-to-union/template.ts
@@ -0,0 +1,10 @@
+type TupleToUnion<T extends any[]> = T[number]
+/**
+ * 1. type TupleToUnion<T> = T extends unknown[] ? T[number] : never
+ * 正确，因为读取了全部元素
+ * 
+ * 2. type TupleToUnion<T> = T extends Array<infer K> ? K : never
+ *  可以利用 infer 和 Array 的泛型
+ */
+
+//  type TupleToUnion<T> = T extends unknown[] ? T[number] : never
